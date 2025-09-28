fe74c7da495cce042e00d3efab334c7822c87490
abdullah yosry <abdullh2004ss@gmail.com>
2025-09-28 18:48:29 +0300

    change animations on landing page + integrate zakat page

--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import { AES256Encryption } from "../utils/encryption";
 
 const API_BASE_URL =
-  "http:\\\\185.207.251.48:8085/ERPDatabaseWorkFunctions";
-
+"https://framework.md-license.com:8093/emsserver.dll/ERPDatabaseWorkFunctions";
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
